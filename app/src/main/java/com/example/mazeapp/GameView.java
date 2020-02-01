806f880a61136ad6d064d996be846aece1ab000c
Yong Wei <yongwei996@gmail.com>
2020-02-01 10:22:00 +0800

    able to create the maze

--- app/src/main/java/com/example/mazeapp/GameView.java
+++ app/src/main/java/com/example/mazeapp/GameView.java
@@ -8,11 +8,16 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
 public class GameView extends View {
     private Cell[][] cells;
     private static final int COLS = 30, ROWS = 7;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
+    private Random random;
     private Paint wallPaint;
 
     public GameView(Context context, AttributeSet attrs) {
@@ -22,7 +27,78 @@ public class GameView extends View {
         wallPaint.setColor(Color.BLACK);
         wallPaint.setStrokeWidth(WALL_THICKNESS);
 
+        random = new Random();
         createMaze();
+    }
+
+
+    //get the neighbour of current cell method
+    private Cell getNeighbour(Cell cell){
+        ArrayList<Cell> neighbours = new ArrayList<>();
+
+        //left neighbour
+        //only allow to check for neighbour if column of cell is greater than 0
+        if(cell.col>0) {
+            if (!cells[cell.col - 1][cell.row].visited) {
+                neighbours.add(cells[cell.col - 1][cell.row]);
+            }
+        }
+
+        //right neighbour
+        //only allow to check for neighbour if column of cell does not exceed max column
+        if(cell.col< COLS-1) {
+            if (!cells[cell.col +1][cell.row].visited) {
+                neighbours.add(cells[cell.col + 1][cell.row]);
+            }
+        }
+
+        //top neighbour
+        //only allow to check for neighbour if column of cell is greater than 0
+        if(cell.row>0) {
+            if (!cells[cell.col][cell.row -1].visited) {
+                neighbours.add(cells[cell.col][cell.row -1]);
+            }
+        }
+
+        //only allow to check for neighbour if column of cell is greater than 0
+        if(cell.row < ROWS -1) {
+            //left neighbour
+            if (!cells[cell.col][cell.row +1 ].visited) {
+                neighbours.add(cells[cell.col][cell.row +1]);
+            }
+        }
+
+        // get a random neighbour
+        if(neighbours.size()>0) {
+            int index = random.nextInt(neighbours.size());
+            return neighbours.get(index);
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    private void removeWall(Cell current, Cell next){
+        if(current.col == next.col && current.row== next.row+1){
+            current.topWall =false;
+            next.bottomWall = false;
+        }
+
+        if(current.col == next.col && current.row== next.row-1){
+            current.bottomWall =false;
+            next.topWall = false;
+        }
+
+        if(current.col == next.col+1 && current.row== next.row){
+            current.leftWall =false;
+            next.rightWall = false;
+        }
+
+        if(current.col == next.col-1 && current.row== next.row){
+            current.rightWall =false;
+            next.leftWall = false;
+        }
     }
 
 
@@ -34,6 +110,25 @@ public class GameView extends View {
                 cells[x][y] = new Cell(x, y);
             }
         }
+
+        //create a stack for the cells
+        Stack<Cell> stack = new Stack<>();
+        Cell current, next;
+
+        //method that will remove the wall and generate the maze
+        current = cells[0][0];
+        do{
+            next= getNeighbour(current);
+            if(next != null){
+                removeWall(current, next);
+                stack.push(current);
+                current = next;
+                current.visited = true;
+            }
+            else {
+                current= stack.pop();
+            }
+        }while (!stack.empty());
 
     }
 
@@ -110,7 +205,8 @@ private class Cell{
                 topWall =true,
                 leftWall = true,
                 rightWall = true,
-                bottomWall = true;
+                bottomWall = true,
+                visited = false;
 
         int col, row;
 
